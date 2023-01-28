e7c0c6bcb2ff8cb1aead6afdc14e728dca9975f6
sumeyyetuzuner17 <sumeyyedeniz34@gmail.com>
2023-01-29 02:07:21 +0300

    Çözüldü ve Anlaşıldı :)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import Karakter from "./components/Karakter";
+import axios from "axios";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [karakters, setKarakters] = useState([]);
+  const [okunanIndex, setOkunanIndex] = useState(0);
 
+
+
+  useEffect(() => {
+    axios.get("https://swapi.dev/api/people/").then((response) => {
+      setKarakters(response.data.results);
+      console.log("burdayım", response.data);
+    });
+  }, []);
+
+  console.log("şşşşşşşşş", okunanIndex)
   return (
     <div className="App">
       <h1 className="Header">Karakterler</h1>
+      {karakters === null ? (
+        <div>Yükleniyor</div>
+      ) : (
+        <div className="chars-list">
+          {karakters.map((k, sira) => (
+            <Karakter
+              kisi={k}
+              kisiIndex={sira}
+              acikMi={sira === okunanIndex}
+              tiklayincaYap={() => setOkunanIndex(sira)}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Karakter.js
+++ src/components/Karakter.js
@@ -1,1 +1,32 @@
 // Karakter bileşeniniz buraya gelecek
+import React from "react";
+import styled from "styled-components";
+
+const SCDiv = styled.div`
+  background-color: transparent;
+  width: 500px;
+  margin: 0 auto;
+`;
+const SCH2 = styled.h2`
+  background-color: transparent;
+  width: 500px;
+  margin: 0 auto;
+`;
+
+const Karakter = (props) => {
+  return (
+    <SCDiv key={props.kisi.url} onClick={props.tiklayincaYap}>
+      <SCH2>{props.kisi.name}</SCH2>
+      {props.acikMi && (
+        <SCDiv className="char-ozellikleri">
+          <SCDiv>Göz rengi {props.kisi.eye_color}</SCDiv>
+          <SCDiv>Boyu: {props.kisi.height}</SCDiv>
+          <SCDiv>Kilosu: {props.kisi.mass}</SCDiv>
+          <SCDiv>Doğum Tarihi: {props.kisi.birth_year}</SCDiv>
+        </SCDiv>
+      )}
+    </SCDiv>
+  );
+};
+
+export default Karakter;
